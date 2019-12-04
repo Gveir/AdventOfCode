cc2419987152aa130319b307535ed2b0e45a70c2
Gveir <kri5s.prog@gmail.com>
2019-12-04 20:42:33 +0100

    AdventOfCode 2019 - puzzle 03

---
+++ AdventOfCode02.Tests/IntcodeTests.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode02;
+using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AgventOfCode02
+{
+    public class IntcodeTests
+    {
+        [Theory]
+        [InlineData("1,0,0,0,99", "2,0,0,0,99")]
+        [InlineData("2,3,0,3,99", "2,3,0,6,99")]
+        [InlineData("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        [InlineData("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        public void Calculate(string input, string expectedOutput)
+        {
+            Assert.Equal(expectedOutput, Intcode.Calculate(input));
+        }
+
+        [Fact]
+        public void ProgramAlarm()
+        {
+            var input = File.ReadAllText("Input.txt").Split(',').Select(i => Convert.ToInt64(i)).ToArray();
+            input[1] = 12;
+            input[2] = 2;
+
+            Assert.StartsWith("2890696,", Intcode.Calculate(string.Join(',', input)));
+        }
+    }
+}
---
+++ AgventOfCode02/Intcode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode02
+{
+    public class Intcode
+    {
+        private const int FinishOpcode = 99;
+
+        public static string Calculate(string input)
+        {
+            var inputs = input.Split(',').Select(i => Convert.ToInt64(i)).ToArray();
+
+            int index = 0;
+
+            while (inputs[index] != FinishOpcode)
+            {
+                var opcode = inputs[index];
+                long result;
+                switch (opcode)
+                {
+                    case 1:
+                        result = inputs[inputs[index + 1]] + inputs[inputs[index + 2]];
+                        break;
+                    case 2:
+                        result = inputs[inputs[index + 1]] * inputs[inputs[index + 2]];
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unrecognized operation opcode: {opcode}");
+                }
+
+                inputs[inputs[index + 3]] = result;
+
+                index += 4;
+            }
+
+            return string.Join(',', inputs);
+        }
+    }
+}
