46460835288a028e77816a444eeadd7ce81dac64
Gveir <kri5s.prog@gmail.com>
2019-12-14 21:48:05 +0100

    AdventOfCode 2019 - puzzle 14

--- AdventOfCode07.Tests/AmplifierControllerSoftwareTests.cs
+++ AdventOfCode07.Tests/AmplifierControllerSoftwareTests.cs
@@ -26,5 +26,25 @@ namespace AdventOfCode07.Tests
 
             Assert.Equal(567045, controller.DetermineMaxThrusterSignal());
         }
+
+        [Theory]
+        [InlineData("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5", 139629729)]
+        [InlineData("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10", 18216)]
+        public void TestExamplesFeedbackLoop(string program, long expectedMaxThrusterSignal)
+        {
+            var controller = new FeedbackLoopedAmplifierController(program);
+
+            Assert.Equal(expectedMaxThrusterSignal, controller.DetermineMaxThrusterSignal());
+        }
+
+        [Fact]
+        public void TestProgramFeedbackLoop()
+        {
+            var program = File.ReadAllText("Program.txt");
+
+            var controller = new FeedbackLoopedAmplifierController(program);
+
+            Assert.Equal(39016654, controller.DetermineMaxThrusterSignal());
+        }
     }
 }
---
+++ AdventOfCode07/FeedbackLoopedAmplifierController.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode02;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode07
+{
+    public class FeedbackLoopedAmplifierController
+    {
+        private readonly string _program;
+
+        public FeedbackLoopedAmplifierController(string program)
+        {
+            _program = program;
+        }
+
+        public long DetermineMaxThrusterSignal()
+        {
+            long maxSignal = 0;
+
+            var phaseSettings = new List<int> { 5, 6, 7, 8, 9 }.GetPermutations(5).ToArray();
+
+            foreach (var phaseSettingSequence in phaseSettings)
+            {
+                var signal = CalculateSignal(phaseSettingSequence);
+
+                maxSignal = signal > maxSignal ? signal : maxSignal;
+            }
+
+            return maxSignal;
+        }
+
+        private long CalculateSignal(IEnumerable<int> phaseSettingSequence)
+        {
+            Intcode[] amplifiers = phaseSettingSequence.Select(phaseSetting => new Intcode(_program, phaseSetting)).ToArray();
+            Intcode ampE = amplifiers.Last();
+
+            int ampIndex = 0;
+            long input = 0;
+
+            while (!ampE.IsFinished)
+            {
+                var amplifier = amplifiers[ampIndex];
+
+                amplifier.EnqueueInput(input);
+
+                amplifier.Process();
+
+                input = amplifier.Output;
+
+                ampIndex = (ampIndex + 1) % amplifiers.Length;
+            }
+
+            return input;
+        }
+    }
+}
