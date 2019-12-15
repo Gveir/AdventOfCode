fe9de18fba7f317187cc5276830d9f16146d3404
Gveir <kri5s.prog@gmail.com>
2019-12-15 12:02:06 +0100

    AdventOfCode 2019 - puzzle 19

---
+++ AdventOfCode10.Tests/MonitoringStationTests.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using Xunit;
+
+namespace AdventOfCode10.Tests
+{
+    public class MonitoringStationTests
+    {
+        [Theory]
+        [InlineData("Example1.txt", 33)]
+        [InlineData("Example2.txt", 35)]
+        [InlineData("Example3.txt", 41)]
+        [InlineData("Example4.txt", 210)]
+        [InlineData("Input.txt", 299)]
+        public void FindBestAsteroid(string exampleFileName, int expectedLargestNumberOfOtherAsteroids)
+        {
+            var map = File.ReadAllText(exampleFileName);
+
+            int largestNumberOfOtherAsteroids = BestPlaceDetector.Determine(map);
+
+            Assert.Equal(expectedLargestNumberOfOtherAsteroids, largestNumberOfOtherAsteroids);
+        }
+    }
+}
---
+++ AdventOfCode10/BestPlaceDetector.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode10
+{
+    internal class Asteroid
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Asteroid(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"X:{X}/Y:{Y}";
+        }
+    }
+
+    public class BestPlaceDetector
+    {
+        public static int Determine(string map)
+        {
+            var asteroids = FindAllAsteroids(map).ToList();
+
+            int maxCount = 0;
+
+            foreach (var asteroid in asteroids)
+            {
+                var quadrants = new List<IEnumerable<Asteroid>>
+                {
+                    asteroids.Where(a => a.X > asteroid.X && a.Y > asteroid.Y),
+                    asteroids.Where(a => a.X > asteroid.X && a.Y <= asteroid.Y),
+                    asteroids.Where(a => a.X <= asteroid.X && a.Y > asteroid.Y),
+                    asteroids.Where(a => a.X <= asteroid.X && a.Y <= asteroid.Y),
+                };
+
+                var count = quadrants.Sum(quadrrantAsteroids => CalculateDetectableAsteroidsCount(asteroid, quadrrantAsteroids));
+
+                maxCount = count > maxCount ? count : maxCount;
+            }
+
+            return maxCount;
+        }
+
+        private static int CalculateDetectableAsteroidsCount(Asteroid asteroid, IEnumerable<Asteroid> asteroids)
+        {
+            HashSet<int> linesOfSight = new HashSet<int>();
+
+            foreach (var a in asteroids.Where(a => a.X != asteroid.X || a.Y != asteroid.Y))
+            {
+                int dx = asteroid.X - a.X;
+                int dy = (asteroid.Y - a.Y) * 1000000;
+                var c = dx != 0 ? dy / dx : int.MaxValue;
+
+                linesOfSight.Add(c);
+            }
+
+            return linesOfSight.Count;
+        }
+
+        private static IEnumerable<Asteroid> FindAllAsteroids(string map)
+        {
+            var lines = map.Split(Environment.NewLine);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (lines[i][j] == '#')
+                    {
+                        yield return new Asteroid(j, i);
+                    }
+                }
+            }
+        }
+    }
+}
