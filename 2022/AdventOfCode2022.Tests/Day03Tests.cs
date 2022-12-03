87c788536e515d88acd02ba2f5421d7e3b541156
Gveir <kri5s.prog@gmail.com>
2022-12-03 12:15:23 +0100

    AdventOfCode 2022 - day 03 part 1&2

---
+++ 2022/AdventOfCode2022.Tests/Day03Tests.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCode2022.Day03;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day03Tests
+    {
+        [Fact]
+        public void PrioritySumTestExample()
+        {
+            var input = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+            var prioritiesSum = PriorityAccumulator.CalculatePrioritySum(input);
+
+            Assert.Equal(157, prioritiesSum);
+        }
+
+        [Theory]
+        [InlineData("aa", 1)]
+        [InlineData("zz", 26)]
+        [InlineData("AA", 27)]
+        [InlineData("ZZ", 52)]
+        public void PrioritySumTestSingleItem(string input, int expectedPrioritySum)
+        {
+            var prioritiesSum = PriorityAccumulator.CalculatePrioritySum(input);
+
+            Assert.Equal(expectedPrioritySum, prioritiesSum);
+        }
+
+        [Fact]
+        public void PrioritySumTestInput()
+        {
+            var input = File.ReadAllText("Input/Day03.txt");
+
+            var prioritiesSum = PriorityAccumulator.CalculatePrioritySum(input);
+
+            Assert.Equal(7581, prioritiesSum);
+        }
+
+        [Fact]
+        public void BadgePrioritySumTestExample()
+        {
+            var input = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+            var prioritiesSum = PriorityAccumulator.CalculateBadgeSum(input);
+
+            Assert.Equal(70, prioritiesSum);
+        }
+
+        [Fact]
+        public void BadgePrioritySumTestInput()
+        {
+            var input = File.ReadAllText("Input/Day03.txt");
+
+            var prioritiesSum = PriorityAccumulator.CalculateBadgeSum(input);
+
+            Assert.Equal(2525, prioritiesSum);
+        }
+    }
+}
---
+++ 2022/AdventOfCode2022/Day03/PriorityAccumulator.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2022.Day03
+{
+    public class PriorityAccumulator
+    {
+        public static int CalculatePrioritySum(string input) =>
+            input.Split(Environment.NewLine).Select(CalculateRucksackPriority).ToArray().Sum();
+
+        public static int CalculateBadgeSum(string input) => 
+            CalculateBadgePriorities(input.Split(Environment.NewLine)).Sum();
+
+        private static int CalculateRucksackPriority(string input)
+        {
+            var commonItem = input.AsSpan(0, input.Length / 2).ToArray().Intersect(input.AsSpan(input.Length / 2, input.Length / 2).ToArray()).Single();
+            return CalculateItemPriority(commonItem);
+        }
+
+        private static IEnumerable<int> CalculateBadgePriorities(string[] rucksacks)
+        {
+            var badgePriorities = new List<int>();
+            for (int i = 0; i < rucksacks.Length; i = i + 3)
+            {
+                var commonItem = rucksacks[i].Intersect(rucksacks[i + 1]).Intersect(rucksacks[i + 2]).Single();
+                badgePriorities.Add(CalculateItemPriority(commonItem));
+            }
+            return badgePriorities;
+        }
+
+        private static int CalculateItemPriority(char item) => item >= 'a' ? item - 'a' + 1 : item - 'A' + 27;
+    }
+}
