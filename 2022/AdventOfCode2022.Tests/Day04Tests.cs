d71b0d8293f5fbadf9652da18a032975c535f613
Gveir <kri5s.prog@gmail.com>
2022-12-04 12:13:52 +0100

    AdventOfCode 2022 - day 04 part 2

--- 2022/AdventOfCode2022.Tests/Day04Tests.cs
+++ 2022/AdventOfCode2022.Tests/Day04Tests.cs
@@ -28,5 +28,30 @@ namespace AdventOfCode2022.Tests
 
             Assert.Equal(599, containedSectionsCount);
         }
+
+        [Fact]
+        public void FindOverlappingSectionsTestExample()
+        {
+            var input = @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+            var containedSectionsCount = SectionsScanner.CountOverlappingSections(input);
+
+            Assert.Equal(4, containedSectionsCount);
+        }
+
+        [Fact]
+        public void FindOverlappingSectionsTestInput()
+        {
+            var input = File.ReadAllText("Input/Day04.txt");
+
+            var containedSectionsCount = SectionsScanner.CountOverlappingSections(input);
+
+            Assert.Equal(928, containedSectionsCount);
+        }
     }
 }
--- 2022/AdventOfCode2022/Day04/SectionsScanner.cs
+++ 2022/AdventOfCode2022/Day04/SectionsScanner.cs
@@ -5,11 +5,21 @@
         public static int CountFullyContainedSections(string input) =>
             input.Split(Environment.NewLine).Select(IsSectionFullyContained).Count(x => x);
 
+        public static int CountOverlappingSections(string input) =>
+            input.Split(Environment.NewLine).Select(AreOverlappingSections).Count(x => x);
         private static bool IsSectionFullyContained(string assignmensPair)
         {
-            var ranges = assignmensPair.Split(',')
-                .Select(a => a.Split('-').Select(int.Parse).ToArray()).ToArray();
+            var ranges = ParseRanges(assignmensPair);
             return ranges[0][0] <= ranges[1][0] && ranges[0][1] >= ranges[1][1] || ranges[1][0] <= ranges[0][0] && ranges[1][1] >= ranges[0][1];
         }
+
+        private static bool AreOverlappingSections(string assignmensPair)
+        {
+            var ranges = ParseRanges(assignmensPair);
+            return ranges[0][0] <= ranges[1][1] && ranges[0][1] >= ranges[1][0] || ranges[1][0] <= ranges[0][1] && ranges[1][1] >= ranges[0][0];
+        }
+
+        private static int[][] ParseRanges(string assignmensPair) => 
+            assignmensPair.Split(',').Select(a => a.Split('-').Select(int.Parse).ToArray()).ToArray();
     }
 }
