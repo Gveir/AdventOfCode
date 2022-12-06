0d15d5ba00f7fe9bf77e6a55c3edb86eac5f9d27
Gveir <kri5s.prog@gmail.com>
2022-12-06 09:06:12 +0100

    AdventOfCode 2022 - day 06 part 1

---
+++ 2022/AdventOfCode2022.Tests/Day06Tests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2022.Day06;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day06Tests
+    {
+        [Theory]
+        [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void FindFirstStartOfPacketMarkerTestExample(string input, int expectedFirstMarker)
+        {
+            var firstMarker = StartOfPacketMarkerFinder.FindFirst(input);
+
+            Assert.Equal(expectedFirstMarker, firstMarker);
+        }
+
+        [Fact]
+        public void FindFirstStartOfPacketMarkerTestInput()
+        {
+            var input = File.ReadAllText("Input/Day06.txt");
+
+            var firstMarker = StartOfPacketMarkerFinder.FindFirst(input);
+
+            Assert.Equal(1542, firstMarker);
+        }
+    }
+}
---
+++ 2022/AdventOfCode2022/Day06/StartOfPacketMarkerFinder.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2022.Day06
+{
+    public class StartOfPacketMarkerFinder
+    {
+        public static int FindFirst(string input)
+        {
+            for (int i = 3; i < input.Length; i++)
+            {
+                char[] chunk = input.AsSpan(i - 3, 4).ToArray();
+                if( chunk.GroupBy(ch => ch).Count() == 4)
+                {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
