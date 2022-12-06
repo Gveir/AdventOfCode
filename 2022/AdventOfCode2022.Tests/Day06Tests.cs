1efde7583aed422d4d295f3288aa40bf602e1cf5
Gveir <kri5s.prog@gmail.com>
2022-12-06 09:15:31 +0100

    AdventOfCode 2022 - day 06 part 2

--- 2022/AdventOfCode2022.Tests/Day06Tests.cs
+++ 2022/AdventOfCode2022.Tests/Day06Tests.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode2022.Tests
         [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
         public void FindFirstStartOfPacketMarkerTestExample(string input, int expectedFirstMarker)
         {
-            var firstMarker = StartOfPacketMarkerFinder.FindFirst(input);
+            var firstMarker = MarkerFinder.FindFirstStartOfPacket(input);
 
             Assert.Equal(expectedFirstMarker, firstMarker);
         }
@@ -22,9 +22,32 @@ namespace AdventOfCode2022.Tests
         {
             var input = File.ReadAllText("Input/Day06.txt");
 
-            var firstMarker = StartOfPacketMarkerFinder.FindFirst(input);
+            var firstMarker = MarkerFinder.FindFirstStartOfPacket(input);
 
             Assert.Equal(1542, firstMarker);
+        }
+
+        [Theory]
+        [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        public void FindFirstStartOfMessageMarkerTestExample(string input, int expectedFirstMarker)
+        {
+            var firstMarker = MarkerFinder.FindFirstStartOfMessage(input);
+
+            Assert.Equal(expectedFirstMarker, firstMarker);
+        }
+
+        [Fact]
+        public void FindFirstStartOfMessageMarkerTestInput()
+        {
+            var input = File.ReadAllText("Input/Day06.txt");
+
+            var firstMarker = MarkerFinder.FindFirstStartOfMessage(input);
+
+            Assert.Equal(3153, firstMarker);
         }
     }
 }
---
+++ 2022/AdventOfCode2022/Day06/MarkerFinder.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2022.Day06
+{
+    public class MarkerFinder
+    {
+        public static int FindFirstStartOfPacket(string input) =>
+            FindFirstMarker(input, 4);
+
+        public static int FindFirstStartOfMessage(string input) =>
+            FindFirstMarker(input, 14);
+
+        private static int FindFirstMarker(string input, int packetLength)
+        {
+            for (int i = packetLength - 1; i < input.Length; i++)
+            {
+                char[] chunk = input.AsSpan(i - (packetLength - 1), packetLength).ToArray();
+                if (chunk.GroupBy(ch => ch).Count() == packetLength)
+                {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
