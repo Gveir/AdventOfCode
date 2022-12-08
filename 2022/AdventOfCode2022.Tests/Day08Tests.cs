019f0c6cf40ce5e4cbd0959cda123671372e9ee9
Gveir <kri5s.prog@gmail.com>
2022-12-08 22:58:55 +0100

    AdventOfCode 2022 - day 08 part 1

---
+++ 2022/AdventOfCode2022.Tests/Day08Tests.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode2022.Day08;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day08Tests
+    {
+        [Theory]
+        [InlineData("Examples/Day08.txt", 21)]
+        [InlineData("Input/Day08.txt", 1647)]
+        public void CountVisibleTreesTest(string inputPath, int expectedVisibleTreesCount)
+        {
+            var input = File.ReadAllLines(inputPath);
+            
+            var visibleTreesCount = Quadcopter.CountVisibleTrees(input);
+
+            Assert.Equal(expectedVisibleTreesCount, visibleTreesCount);
+        }
+    }
+}
---
+++ 2022/AdventOfCode2022/Day08/Quadcopter.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2022.Day08
+{
+    public class Quadcopter
+    {
+        public static int CountVisibleTrees(string[] input)
+        {
+            var map = GenerateMap(input).ToList();
+
+            return map.Where(tree => IsTreeVisible(map, tree)).Count();
+        }
+
+        private static IEnumerable<Tree> GenerateMap(string[] input)
+        {
+            for (int row = 0; row < input.Length; row++)
+            {
+                for (int column = 0; column < input[row].Length; column++)
+                {
+                    yield return new Tree(row, column, input[row][column] - '0');
+                }
+            }
+        }
+
+        private static bool IsTreeVisible(IEnumerable<Tree> map, Tree tree)
+        {
+            var isVisibleFromLeft = map.Where(t => t.Row == tree.Row && t.Column < tree.Column).All(t => t.Height < tree.Height);
+            var isVisibleFromRight = map.Where(t => t.Row == tree.Row && t.Column > tree.Column).All(t => t.Height < tree.Height);
+            var isVisibleFromTop = map.Where(t => t.Row < tree.Row && t.Column == tree.Column).All(t => t.Height < tree.Height);
+            var isVisibleFromBottom = map.Where(t => t.Row > tree.Row && t.Column == tree.Column).All(t => t.Height < tree.Height);
+            return isVisibleFromLeft || isVisibleFromRight || isVisibleFromTop || isVisibleFromBottom;
+        }
+    }
+
+    internal record Tree (int Row, int Column, int Height);
+}
