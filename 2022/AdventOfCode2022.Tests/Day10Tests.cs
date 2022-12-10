e122edbfc7331c6796894818c964f83eac921c0d
Gveir <kri5s.prog@gmail.com>
2022-12-10 11:59:28 +0100

    AdventOfCode 2022 - day 10 part 1

---
+++ 2022/AdventOfCode2022.Tests/Day10Tests.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode2022.Day10;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day10Tests
+    {
+        [Theory]
+        [InlineData("Examples/Day10.txt", 13140)]
+        [InlineData("Input/Day10.txt", 13820)]
+        public void CalculateSignalStrengthsSumTest(string inputPath, int expectedSignalsStrengthsSum)
+        {
+            var input = File.ReadAllLines(inputPath);
+
+            var signalsStrengthsSum = Cpu.CalculateSignalsStrengthsSum(input,
+                new int[] { 20, 60, 100, 140, 180, 220 });
+
+            Assert.Equal(expectedSignalsStrengthsSum, signalsStrengthsSum);
+        }
+    }
+}
---
+++ 2022/AdventOfCode2022/Day10/Cpu.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2022.Day10
+{
+    public class Cpu
+    {
+        private int _x = 1;
+        private IList<int> _registerHistory = new List<int>();
+
+        public static int CalculateSignalsStrengthsSum(string[] input, int[] cyclesToSum)
+        {
+            var cpu = new Cpu();
+
+            cpu.ProcessInstructions(input);
+
+            return cyclesToSum.Select(c => cpu._registerHistory[c - 1] * c).Sum();
+        }
+
+        private void ProcessInstructions(string[] input)
+        {
+            foreach (var instruction in input)
+            {
+                _registerHistory.Add(_x);
+                if (instruction == "noop")
+                {
+                    continue;
+                }
+
+                _registerHistory.Add(_x);
+                var addValue = int.Parse(instruction.Split(' ')[1]);
+                _x += addValue;
+            }
+        }
+    }
+}
