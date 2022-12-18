54e0a3fa8dcd0edec30a1b0c8d08b3f34c721669
Gveir <kri5s.prog@gmail.com>
2022-12-19 00:42:54 +0100

    AdventOfCode 2022 - day 18 part 1

---
+++ 2022/AdventOfCode2022.Tests/Day18Tests.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode2022.Day18;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day18Tests
+    {
+        [Theory]
+        [InlineData("Examples/Day18.txt", 10)]
+        [InlineData("Examples/Day18.2.txt", 64)]
+        [InlineData("Input/Day18.txt", 4536)]
+        public void Test(string inputPath, int expectedSurfaceArea)
+        {
+            var input = File.ReadAllLines(inputPath);
+
+            var surfaceArea = LavaDropletAnalyzer.CalculateSurfaceArea(input);
+
+            Assert.Equal(expectedSurfaceArea, surfaceArea);
+        }
+    }
+}
---
+++ 2022/AdventOfCode2022/Day18/LavaDropletAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2022.Day18
+{
+    public class LavaDropletAnalyzer
+    {
+        private static readonly List<Cube> Neighbours = new List<Cube>
+        {
+            new Cube(-1, 0, 0),
+            new Cube(1, 0, 0),
+            new Cube(0, -1, 0),
+            new Cube(0, 1, 0),
+            new Cube(0, 0, -1),
+            new Cube(0, 0, 1)
+        };
+
+        public static int CalculateSurfaceArea(string[] input)
+        {
+            var cubes = ParseInput(input).ToHashSet();
+
+            return cubes.Select(cube => CalculateSurfaceArea(cube, cubes)).Sum();
+        }
+
+        private static int CalculateSurfaceArea(Cube cube, HashSet<Cube> cubes) =>
+            Neighbours.Select(n => cube + n).Where(n => !cubes.Contains(n)).Count();
+
+        private static IEnumerable<Cube> ParseInput(string[] input)
+        {
+            foreach (var inputLine in input)
+            {
+                var position = inputLine.Split(',').Select(c => int.Parse(c)).ToList();
+                yield return new Cube(position[0], position[1], position[2]);
+            }
+        }
+    }
+
+    internal record Cube(int X, int Y, int Z)
+    {
+        public static Cube operator+(Cube a, Cube b) => new Cube(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+}
