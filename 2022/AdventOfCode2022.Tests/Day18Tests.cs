32abb6227b85c328e1301a20d246b99fd269d044
Gveir <kri5s.prog@gmail.com>
2022-12-19 03:02:36 +0100

    [WIP] AdventOfCode 2022 - day 18 part 2

--- 2022/AdventOfCode2022.Tests/Day18Tests.cs
+++ 2022/AdventOfCode2022.Tests/Day18Tests.cs
@@ -8,11 +8,26 @@ namespace AdventOfCode2022.Tests
         [InlineData("Examples/Day18.txt", 10)]
         [InlineData("Examples/Day18.2.txt", 64)]
         [InlineData("Input/Day18.txt", 4536)]
-        public void Test(string inputPath, int expectedSurfaceArea)
+        public void CalculateSurfaceAreaTest(string inputPath, int expectedSurfaceArea)
         {
             var input = File.ReadAllLines(inputPath);
 
             var surfaceArea = LavaDropletAnalyzer.CalculateSurfaceArea(input);
+
+            Assert.Equal(expectedSurfaceArea, surfaceArea);
+        }
+
+        [Theory]
+        [InlineData("Examples/Day18.2.txt", 58)]
+        [InlineData("Examples/Day18.extra.txt", 96)]
+        [InlineData("Examples/Day18.extra2.txt", 112)]
+        [InlineData("Examples/Day18.extra3.txt", 112)]
+        [InlineData("Input/Day18.txt", 4536)]
+        public void CalculateExteriorSurfaceAreaTest(string inputPath, int expectedSurfaceArea)
+        {
+            var input = File.ReadAllLines(inputPath);
+
+            var surfaceArea = LavaDropletAnalyzer.CalculateExteriorSurfaceArea(input);
 
             Assert.Equal(expectedSurfaceArea, surfaceArea);
         }
--- 2022/AdventOfCode2022/Day18/LavaDropletAnalyzer.cs
+++ 2022/AdventOfCode2022/Day18/LavaDropletAnalyzer.cs
@@ -1,8 +1,10 @@
-﻿namespace AdventOfCode2022.Day18
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2022.Day18
 {
     public class LavaDropletAnalyzer
     {
-        private static readonly List<Cube> Neighbours = new List<Cube>
+        public static readonly List<Cube> Neighbours = new List<Cube>
         {
             new Cube(-1, 0, 0),
             new Cube(1, 0, 0),
@@ -16,8 +18,56 @@
         {
             var cubes = ParseInput(input).ToHashSet();
 
-            return cubes.Select(cube => CalculateSurfaceArea(cube, cubes)).Sum();
+            return CalculateSurfaceArea(cubes);
         }
+
+
+        public static int CalculateExteriorSurfaceArea(string[] input)
+        {
+            var cubes = ParseInput(input).ToHashSet();
+
+            var surfaceArea = CalculateSurfaceArea(cubes);
+
+            var (minX, maxX) = cubes.MinAndMax(cube => cube.X);
+            var (minY, maxY) = cubes.MinAndMax(cube => cube.Y);
+            var (minZ, maxZ) = cubes.MinAndMax(cube => cube.Z);
+
+            for (int x = minX; x <= maxX; x++)
+            {
+                for (int y = minY; y <= maxY; y++)
+                {
+                    for (int z = minZ; z < maxZ; z++)
+                    {
+                        var cube = new Cube(x, y, z);
+
+                        if(!cubes.Contains(cube) && IsSurrounded(cube, cubes))
+                        {
+                            surfaceArea -= CountNeighbours(cube, cubes);
+                        }
+                    }
+                }
+            }
+
+            return surfaceArea;
+        }
+
+        private static bool IsSurrounded(Cube cube, HashSet<Cube> cubes)
+        {
+            var x = cubes.Any(c => c.X < cube.X && c.Y == cube.Y && c.Z == cube.Z) &&
+                cubes.Any(c => c.X > cube.X && c.Y == cube.Y && c.Z == cube.Z);
+            var y = cubes.Any(c => c.X == cube.X && c.Y < cube.Y && c.Z == cube.Z) &&
+                cubes.Any(c => c.X == cube.X && c.Y > cube.Y && c.Z == cube.Z);
+            var z = cubes.Any(c => c.X == cube.X && c.Y == cube.Y && c.Z < cube.Z) &&
+                cubes.Any(c => c.X == cube.X && c.Y == cube.Y && c.Z > cube.Z);
+
+            return x && y && z;
+        }
+
+        private static int CountNeighbours(Cube cube, HashSet<Cube> cubes) =>
+            Neighbours.Select(n => cube + n).Where(n => cubes.Contains(n)).Count();
+
+        private static int CalculateSurfaceArea(HashSet<Cube> cubes) =>
+            cubes.Select(cube => CalculateSurfaceArea(cube, cubes)).Sum();
 
         private static int CalculateSurfaceArea(Cube cube, HashSet<Cube> cubes) =>
             Neighbours.Select(n => cube + n).Where(n => !cubes.Contains(n)).Count();
@@ -32,8 +82,15 @@
         }
     }
 
-    internal record Cube(int X, int Y, int Z)
+    [DebuggerDisplay("{X} {Y} {Z}")]
+    public record Cube(int X, int Y, int Z)
     {
         public static Cube operator+(Cube a, Cube b) => new Cube(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+
+    internal static class EnumerableExtensions
+    {
+        public static (int min, int max) MinAndMax<T>(this IEnumerable<T> objects, Func<T, int> selector) => 
+            objects.Aggregate((MinValue: int.MaxValue, MaxValue: int.MinValue), (acc, o) => (Math.Min(acc.MinValue, selector(o)), Math.Max(acc.MaxValue, selector(o))));
     }
 }
